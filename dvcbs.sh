484f43cf4039aee04be2c972262bacbc1533f2b0
Wire <whyyoulittle333@gmail.com>
2020-06-05 23:30:36 -0500

    Much better script
    Split into function and can overall do more.

--- dvcbs.sh
+++ dvcbs.sh
@@ -13,8 +13,12 @@ function help()
    echo "This simple script will convert any OTA to one with a new update engine, SSH key, and build number. Experimental."
    echo "[-h|-o {dir}|-n {dir}]"
    echo "-h         This message"
-   echo "-o {dir}   directory to any latest.ota under 1.0(not working/experimental)"
    echo "-n {dir}   directory to any latest.ota over 1.0/under 1.6. will turn into a 1.8.0.4000d which will work with chipper-dev"
+   echo "-t {dir}   directory to any latest.ota you want to just beta test. it will only edit update engine/ssh key and won't mess with version number."
+   echo "-m {dir}   directory to any latest.ota you want to mount"
+   echo "-b {dir}   directory to an apq8009-robot-sysfs.img you want to build"
+   echo "-mn {dir} this will mount an OTA's sysfs and copy over update-engine, ssh key, and version stuff"
+   echo "-mt {dir} this will mount an OTA's sysfs and copy over update-engine and ssh key"
    exit 0
 }
 
@@ -25,28 +29,25 @@ function ctrl_c() {
     exit 1 
 }
 
-#function old()
-#{
-#  echo "Converting OTA in $dir!"
-#  sudo mv ${dir}latest.ota ${dir}latest.tar
-#  sudo tar -xf ${dir}latest.tar --directory ${dir}
-#  sudo mkdir ${dir}edits
-#  sudo openssl enc -d -aes-256-ctr -pass file:ota.pas -md md5 -in ${dir}apq8009-robot-sysfs.img.gz -out ${dir}apq8009-robot-sysfs.img.dec.gz
-#  sudo gzip -d ${dir}apq8009-robot-sysfs.img.dec.gz
-#  sudo mv ${dir}apq8009-robot-sysfs.img.dec ${dir}apq8009-robot-sysfs.img
-#  sudo mount -o loop,rw,sync ${dir}apq8009-robot-sysfs.img ${dir}edits
-#  sudo cp update-engine ${dir}edits/anki/bin/
-#  sudo cp authorized_keys ${dir}edits/etc/ssh/
-#  sudo rm ${dir}apq8009-robot-sysfs.img.gz
-#  sudo gzip -k ${dir}apq8009-robot-sysfs.img
-#  sudo mkdir ${dir}final
-#  sudo openssl enc -e -aes-256-ctr -pass file:ota.pas -md md5 -in ${dir}apq8009-robot-sysfs.img.gz -out ${dir}final/apq8009-robot-sysfs.img.dec.gz
-#  sudo mv ${dir}final/apq8009-robot-sysfs.img.dec.gz ${dir}/final/apq8009-robot.sysfs.img.gz
-#  sudo umount ${dir}edits
-#  sha256sum ${dir}apq8009-robot-sysfs.img
-#}
+function copytest()
+{
+  echo "Copying files over"
+  sudo cp ${refo}/update-engine ${dir}edits/anki/bin/
+  sudo cp ${refo}/authorized_keys ${dir}edits/etc/ssh/
+}
 
-function new()
+function copynew()
+{
+  sudo cp ${refo}/update-engine ${dir}edits/anki/bin/
+  sudo cp ${refo}/authorized_keys ${dir}edits/etc/ssh/
+  sudo cp ${refo}/os-version ${dir}edits/etc/
+  sudo cp ${refo}/os-version-base ${dir}edits/etc/
+  sudo cp ${refo}/os-version-code ${dir}edits/etc/
+  sudo cp ${refo}/version ${dir}edits/anki/etc/
+  sudo cp ${refo}/build.prop ${dir}edits/
+}
+
+function mount()
 {
   echo "Converting OTA in $dir!"
   sudo mv ${dir}latest.ota ${dir}latest.tar
@@ -59,15 +60,12 @@ function new()
   sudo mv ${dir}apq8009-robot-sysfs.img.dec ${dir}apq8009-robot-sysfs.img
   echo "Mounting IMG"
   sudo mount -o loop,rw,sync ${dir}apq8009-robot-sysfs.img ${dir}edits
-  echo "Copying files over"
-  sudo cp ${refo}/update-engine ${dir}edits/anki/bin/
-  sudo cp ${refo}/authorized_keys ${dir}edits/etc/ssh/
-  sudo cp ${refo}/os-version ${dir}edits/etc/
-  sudo cp ${refo}/os-version-base ${dir}edits/etc/
-  sudo cp ${refo}/os-version-code ${dir}edits/etc/
-  sudo cp ${refo}/version ${dir}edits/anki/etc/
-  sudo cp ${refo}/build.prop ${dir}edits/
+  echo "Deleting GZ file for build function to work"
   sudo rm ${dir}apq8009-robot-sysfs.img.gz
+}
+
+function build()
+{
   echo "Compressing. This may take a minute."
   sudo gzip -k ${dir}apq8009-robot-sysfs.img
   sudo mkdir ${dir}final
@@ -76,7 +74,7 @@ function new()
   sudo umount ${dir}edits
   echo "Figuring out SHA256 sum and putting it into manifest."
   sysfssum=$(sha256sum ${dir}apq8009-robot-sysfs.img | head -c 64)
-  printf '%s\n' '[META]' 'manifest_version=1.0.0' 'update_version=1.8.0.4000d' 'ankidev=1' 'num_images=2' '[BOOT]' 'encryption=1' 'delta=0' 'compression=gz' 'wbits=31' 'bytes=13795328' 'sha256='${bootsum} '[SYSTEM]' 'encryption=1' 'delta=0' 'compression=gz' 'wbits=31' 'bytes=608743424' 'sha256='${sysfssum} >${refo}/manifest.ini
+  sudo printf '%s\n' '[META]' 'manifest_version=1.0.0' 'update_version=1.8.0.4000d' 'ankidev=1' 'num_images=2' '[BOOT]' 'encryption=1' 'delta=0' 'compression=gz' 'wbits=31' 'bytes=13795328' 'sha256='${bootsum} '[SYSTEM]' 'encryption=1' 'delta=0' 'compression=gz' 'wbits=31' 'bytes=608743424' 'sha256='${sysfssum} >${refo}/manifest.ini
   echo "Putting into tar."
   sudo tar -C ${refo} -cvf ${refo}/temp.tar manifest.ini
   sudo tar -C ${refo} -rf ${refo}/temp.tar apq8009-robot-boot.img.gz
@@ -102,13 +100,35 @@ if [ $# -gt 0 ]; then
 	-h)
 	    help
             ;;
-#	-o)
-#  	    dir=$2
-#            old
-#            ;;
         -n)
             dir=$2
-            new
+	    mount
+            copynew
+            build
             ;;
+	-t) 
+	    dir=$2
+	    mount
+	    copytest
+            build
+	    ;;
+	-m) 
+	    dir=$2
+	    mount
+	    ;;
+	-b) 
+	    dir=$2
+	    build
+	    ;;
+	-mn) 
+	    dir=$2
+	    mount
+	    copynew
+	    ;;
+	-mt) 
+	    dir=$2
+	    mount
+            copytest
+	    ;;
     esac
 fi
